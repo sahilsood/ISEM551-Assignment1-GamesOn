9ebeddbb35bac4f7f3027abed901b1b1fb4bdaab
Sahil Sood <sahil28sood@gmail.com>
2023-11-27 17:55:43 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ISEM551_Assignment1_GamesOn.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
