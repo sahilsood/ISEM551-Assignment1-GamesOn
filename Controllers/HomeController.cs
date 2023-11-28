376336a4ff4ea283b11d053431374eadc71f6c35
Sahil Sood <sahil28sood@gmail.com>
2023-11-27 21:12:36 -0500

    Modified Home Page, Controller, Css to show Games List

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,52 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        // Sample data for top played games on Xbox
+        var xboxTopGames = new List<GameViewModel>
+        {
+            new GameViewModel
+            {
+                Title = "Halo Infinite", Platform = "Xbox",
+                ImageUrl = "https://seeklogo.com/images/H/halo-infinite-logo-FBA50491EE-seeklogo.com.png"
+            },
+            new GameViewModel
+            {
+                Title = "Forza Horizon 5", Platform = "Xbox",
+                ImageUrl = "https://seeklogo.com/images/F/forza-horizon-logo-168F677383-seeklogo.com.png"
+            },
+            new GameViewModel
+            {
+                Title = "Minecraft", Platform = "Xbox",
+                ImageUrl = "https://seeklogo.com/images/M/minecraft-logo-ECF7D3941A-seeklogo.com.png"
+            },
+        };
+
+        // Sample data for top played games on PlayStation
+        var playstationTopGames = new List<GameViewModel>
+        {
+            new GameViewModel
+            {
+                Title = "God of War", Platform = "PlayStation",
+                ImageUrl = "https://seeklogo.com/images/G/god-of-war-4-logo-75E6099631-seeklogo.com.png"
+            },
+            new GameViewModel
+            {
+                Title = "The Last of Us Part II", Platform = "PlayStation",
+                ImageUrl = "https://seeklogo.com/images/T/the-last-of-us-logo-BAEC7187B5-seeklogo.com.png"
+            },
+            new GameViewModel
+            {
+                Title = "Spider-Man: Miles Morales", Platform = "PlayStation",
+                ImageUrl = "https://seeklogo.com/images/S/spider-man-logo-4EA649A533-seeklogo.com.png"
+            },
+        };
+
+        var model = new HomeViewModel
+        {
+            XboxTopGames = xboxTopGames,
+            PlayStationTopGames = playstationTopGames
+        };
+        return View(model);
     }
 
     public IActionResult Privacy()
