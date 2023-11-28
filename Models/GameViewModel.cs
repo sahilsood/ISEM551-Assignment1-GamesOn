f42d43927ef72faf17f500b5e457f8dea5ff464c
Sahil Sood <sahil28sood@gmail.com>
2023-11-27 21:10:08 -0500

    Added Model classes for Games

---
+++ Models/GameViewModel.cs
@@ -0,0 +1,8 @@
+namespace ISEM551_Assignment1_GamesOn.Models;
+
+public class GameViewModel
+{
+    public string Title { get; set; }
+    public string Platform { get; set; }
+    public string ImageUrl { get; set; }
+}
